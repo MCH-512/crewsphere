7437d9912e8923d6a31f7b9e83707afd2b4e924c
Chabouha Mahmoud <Chabouha.mahmoud@gmail.com>
2025-07-07 22:11:20 +0000

    Parfait

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,6 +2,7 @@
 "use client";
 
 import * as React from "react";
+import Image from "next/image";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
