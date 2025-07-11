6f3e3d8ffb38858b8e102bbece9fdabaa807a673
Chabouha Mahmoud <Chabouha.mahmoud@gmail.com>
2025-07-11 12:41:58 +0000

    le probleme persiste encore , les session crer ne saffichent pas , verfi

--- src/app/my-logbook/page.tsx
+++ src/app/my-logbook/page.tsx
@@ -62,7 +62,7 @@ export default function MyLogbookPage() {
                     else if (data.pilotIds?.includes(user.uid)) userRoleOnFlight = "Pilot";
                     else if (data.cabinCrewIds?.includes(user.uid)) userRoleOnFlight = "Cabin Crew";
                     else if (data.instructorIds?.includes(user.uid)) userRoleOnFlight = "Instructor";
-                    else if (data.traineeIds?.includes(user.uid)) userRoleOnFlight = "Trainee";
+                    else if (data.traineeIds?.includes(user.uid)) userRoleOnFlight = "Stagiaire";
 
 
                     return { ...data, flightDurationMinutes, userRoleOnFlight };
