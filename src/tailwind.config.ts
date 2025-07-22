67812880e7fe87273c76f5af1d3505f6607be1fa
Chabouha Mahmoud <Chabouha.mahmoud@gmail.com>
2025-07-22 10:24:15 +0000

    oui

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -11,8 +11,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Poppins', 'sans-serif'],
-        headline: ['Poppins', 'sans-serif'],
+        body: ['Inter', 'sans-serif'],
+        headline: ['Inter', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
