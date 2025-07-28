532912b5862658ce4ad3985e51a23dcd93d852de
Chabouha Mahmoud <Chabouha.mahmoud@gmail.com>
2025-07-28 09:05:07 +0000

    The error "ReferenceError: useRouter is not defined" indicates that the

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,6 +3,7 @@
 
 import * as React from "react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
