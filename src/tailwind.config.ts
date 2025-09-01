25b0fc423f2c096dc8553dbe25fe0265bb435533
Chabouha Mahmoud <Chabouha.mahmoud@gmail.com>
2025-09-01 13:49:16 +0000

    pourqoui le backgroand de la barre laterale est toujours blanche

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -66,6 +66,7 @@ const config = {
         },
         sidebar: {
           DEFAULT: "hsl(var(--sidebar-background))",
+          background: "hsl(var(--sidebar-background))",
           foreground: "hsl(var(--sidebar-foreground))",
           primary: "hsl(var(--sidebar-primary))",
           "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
