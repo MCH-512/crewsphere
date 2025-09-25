1d1a46034c44a1dbefa74454f6fed983c000448f
Chabouha Mahmoud <Chabouha.mahmoud@gmail.com>
2025-09-25 12:49:33 +0000

    studio-5290892546:~/studio{master}$ npm audit fix --force npm warn using

--- src/next.config.js
+++ src/next.config.js
@@ -17,15 +17,6 @@ const cspHeader = `
 `.replace(/\s{2,}/g, ' ').trim();
 
 const nextConfig = {
-  // Expose Firebase config to the client-side code
-  env: {
-    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  },
   // Allow requests from Firebase Studio's preview domains
   allowedDevOrigins: [
     '*.cloudworkstations.dev'
